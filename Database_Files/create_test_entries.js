976d72f3787f1d5a19d7d4ef753db9a41c8a5756
Ayush <ayushspark11@gmail.com>
2018-04-16 17:21:32 -0400

    expanding js create script

--- Database_Files/create_test_entries.js
+++ Database_Files/create_test_entries.js
@@ -28,5 +28,41 @@ function getRoomId(event, cb) {
 
 //options for dog room
 getRoomId("You meet a dog (7 HP). The dog barks loudly at you, and looks like he is going to attack.", function(result) {
-    con.query("INSERT INTO options VALUE ()");
+    var option1 = false;
+    var option2 = false;
+    var option3 = false;
+    var option4 = false;
+    con.query("INSERT INTO options VALUE (NULL, 'Attempt to answer the riddle', "+result+")", function(err, result) {
+        if(err) {
+            throw err;
+        } else {
+            //option1 = true;
+            con.query("INSERT INTO outcomes VALUE("",)");
+        }
+    });
+    con.query("INSERT INTO options VALUE (NULL, 'Ask him to let you go', "+result+")",function(err, result) {
+        if(err) {
+            throw err;
+        } else {
+            option2 = true;
+        }
+    });
+    con.query("INSERT INTO options VALUE (NULL, 'Attempt to kill the gnome', "+result+")",function(err, result) {
+        if(err) {
+            throw err;
+        } else {
+            option3 = true;
+        }
+    });
+    con.query("INSERT INTO options VALUE (NULL, 'Say \“How about no.\” and flee ... um, continue on to the next room', "+result+")",function(err, result) {
+        if(err) {
+            throw err;
+        } else {
+            option4 = true;
+        }
+    });
+    while(!option1 || !option2 || !option3 || !option4) {
+        //do nothing
+    }
+    con.query
 });
