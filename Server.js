1ea84d552ad3cae1228f6244faab2e3b0369aeb1
Alexander Ferrard <aferrard@purdue.edu>
2018-04-18 15:31:41 -0400

    Cookies, start page, and starting bugs fixed

--- Server.js
+++ Server.js
@@ -4,9 +4,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
-//var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 
-//app.use(cookieParser());
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -25,6 +25,14 @@ app.set('view engine', 'ejs');
 //user res.render to load up an ejs view file
 
 app.get('/', function(req,res){
+    res.cookie('hero', '', {expires: new Date(0)}); // remove user from the cookie
+    res.cookie('health', '', {expires: new Date(0)}); // remove pass from the cookie
+    res.cookie('gold', '', {expires: new Date(0)});
+    res.cookie('weapon', '', {expires: new Date(0)});
+    res.cookie('item', '', {expires: new Date(0)});
+    res.cookie('magik', '', {expires: new Date(0)});
+    res.cookie('roomsCopy', '', {expires: new Date(0)});
+    //console.log(req.cookies);
     res.render('pages/home');
 });
 
@@ -35,7 +43,26 @@ app.get('/leaderboard', function(req,res){
 app.get('/champ', function(req,res){
     res.render('pages/champ');
 });
+var roomsMaster = ["GNOME", "DOG", "HOLE", "CHEST", "POTIONS", "DARKWIZARD", "SEASHELL", "CAMPFIRE", "HELICOPTER", "SALESMAN", "FEAST", "CHOOSEAROOM"];
+
+app.get('/start', function(req,res){
+    res.cookie('hero', req.query.hero, {maxAge: 9000000});
+    res.cookie('health', 50, {maxAge: 9000000});
+    res.cookie('gold', 50, {maxAge: 9000000});
+    res.cookie('weapon', "Stick", {maxAge: 9000000});
+    res.cookie('item', "None", {maxAge: 9000000});
+    res.cookie('magik', "None", {maxAge: 9000000});
+    res.cookie('roomsCopy', roomsMaster, {maxAge: 9000000});
+    res.render('pages/start');
+});
+
 app.get('/room', function(req,res){
+    console.log(req.cookies);
+    var roomsCopy = req.cookies.roomsCopy;
+    var roomPick = roomsCopy.length*Math.random();
+    var room = roomsCopy[roomPick];
+    roomsCopy.splice(roomPick, 1);
+    res.cookie('roomsCopy', roomsCopy, {maxAge: 9000000});
     res.render('pages/room');
 });
 app.get('/outcome', function(req,res){
