0a21559d692337a38dca004eee757235f8596b36
Ayush <ayushspark11@gmail.com>
2018-04-20 17:10:30 -0400

    added functions to get rooms, choices, outcomes

--- Controllers/Connection.js
+++ Controllers/Connection.js
@@ -25,3 +25,39 @@ function addWinner(username, health, money, weapon, item, magik, cb) {
         }
     });
 }
+
+exports.getRoom = getRoom;
+function getRoom(title, cb) {
+    con.query("SELECT * FROM rooms WHERE title = '" + title + "'", function(err, room) {
+        if(err) {
+            cb(err);
+        } else {
+            var z = JSON.parse(JSON.stringify(room[0]));
+            cb(z);
+        }
+    });
+}
+
+exports.getChoices = getChoices;
+function getChoices(roomid, cb) {
+    con.query("SELECT * FROM choices WHERE rooms_room_id = " + roomid, function(err, choices) {
+        if(err) {
+            cb(err);
+        } else {
+            var z = JSON.parse(JSON.stringify(choices));
+            cb(z);
+        }
+    });
+}
+
+exports.getOutcomes = getOutcomes;
+function getOutcomes(choiceid, path, cb) {
+    con.query("SELECT outcome FROM outcomes WHERE choices_choice_id = " + choiceid + " AND outcome_path = '" + path + "'", function(err, outcome) {
+        if(err) {
+            cb(err);
+        } else {
+            var z = JSON.parse(JSON.stringify(outcomes[0].outcome));
+            cb(z);
+        }
+    });
+}
